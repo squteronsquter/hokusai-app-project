7189cf0fbbf3f018701857a367587c5553eb6fdd
Digital Mind <jremesz@gmail.com>
2022-06-18 16:48:53 +0200

    image responsive

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1>Hokusai App!</h1>
+      <h1>Hokusai App</h1>
+      <Image
+        src="/images/arts/wind.png"
+        width="800"
+        height="500"
+        layout="responsive"
+      />
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores
         dolorum rerum aut quis sit sunt maiores tempore ullam modi, repellat
